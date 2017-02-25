046e5d174d93724cba1d45804a79a5e62aa004fa
Eric Pinzur <ericpinzur@otherinbox.com>
2017-02-25 12:15:24 -0600

    updated stuff

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,6 +11,38 @@ router.get('/', function(req, res, next) {
           'GET|POST /rooms/:room_id/controls/:control_id/options \n';
 
   res.send(psuedoHal);
+});
+
+router.get('/audio/*', function(req, res, next) {
+    var path = 'http://localhost:3002/' + req.path.substring(7);
+    console.log('requesting:  ' + path);
+    request(path, function (error, response, body) {
+        res.status(response.statusCode).send(body);
+    });
+});
+
+router.get('/lights/*', function(req, res, next) {
+    var path = 'http://localhost:3001/' + req.path.substring(8);
+    console.log('requesting:  ' + path);
+    request(path, function (error, response, body) {
+        res.status(response.statusCode).send(body);
+    });
+});
+
+router.post('/audio/*', function(req, res, next) {
+    var path = 'http://localhost:3002/' + req.path.substring(7);
+    console.log('posting to:  ' + path);
+    request.post({url:path, form: req.body}, function(err,response,body) {
+        res.status(response.statusCode).send(body);
+    });
+});
+
+router.post('/lights/*', function(req, res, next) {
+    var path = 'http://localhost:3001/' + req.path.substring(8);
+    console.log('posting to:  ' + path);
+    request.post({url:path, form: req.body}, function(err,response,body) {
+        res.status(response.statusCode).send(body);
+    });
 });
 
 module.exports = router;
